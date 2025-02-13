fb92e1b6f42926ed0a834d9a3bfd35873d4b34c4
JungSik Jeong <wndtlr1024@gmail.com>
2025-02-13 10:20:06 +0900

    refactor: zod union -> discriminatedUnion으로 변경해서 타입 구분 더 강하게

--- src/renderer/src/pages/WorkPage/schema.ts
+++ src/renderer/src/pages/WorkPage/schema.ts
@@ -1,12 +1,13 @@
-import { z } from "zod";
+import { z } from 'zod'
 
-export const workSchema = z.union([
+export const workSchema = z.discriminatedUnion('type', [
   z.object({
-    type: z.literal("feed"),
+    type: z.literal('feed')
   }),
   z.object({
-    type: z.literal("hashtag"),
-    tag: z.string({ required_error: "태그를 입력해주세요" }).min(1, "태그를 입력해주세요"),
+    type: z.literal('hashtag'),
+    tag: z.string({ required_error: '태그를 입력해주세요' }).min(1, '태그를 입력해주세요')
   })
 ])
+
 export type WorkSchema = z.infer<typeof workSchema>
