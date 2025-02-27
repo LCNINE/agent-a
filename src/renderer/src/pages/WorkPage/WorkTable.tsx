faa1a7a51357c6d96a255c550da6dd64bdfed82c
JungSik Jeong <wndtlr1024@gmail.com>
2025-02-27 15:55:27 +0900

    styles: 작업 테이블 스크롤처리 및 유저 드랍다운 메뉴 닫히게 설정

--- src/renderer/src/pages/WorkPage/WorkTable.tsx
+++ src/renderer/src/pages/WorkPage/WorkTable.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 import {
   Table,
   TableBody,
@@ -6,60 +6,77 @@ import {
   TableCell,
   TableHead,
   TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-import { useTranslation } from "react-i18next"
-import { useWorkStore, Work } from "@/store/workStore"
-import { Button } from "@/components/ui/button"
-import { ChevronDownIcon, ChevronUpIcon, SquarePenIcon, TrashIcon } from "lucide-react"
-import { WorkFormDialog } from "./WorkFormDialog"
-
+  TableRow
+} from '@/components/ui/table'
+import { useTranslation } from 'react-i18next'
+import { useWorkStore, Work } from '@/store/workStore'
+import { Button } from '@/components/ui/button'
+import { ChevronDownIcon, ChevronUpIcon, SquarePenIcon, TrashIcon } from 'lucide-react'
+import { WorkFormDialog } from './WorkFormDialog'
+import { ScrollArea } from '@renderer/components/ui/scroll-area'
 
 export function WorkTable() {
   const { workList, up, down, remove } = useWorkStore()
-
   const { t } = useTranslation()
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>종류</TableHead>
-          <TableHead>상세</TableHead>
-          <TableHead className="w-[100px]"></TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {
-          workList.map((work) => (
-            <TableRow key={work.id}>
-              <TableCell>{t(`work.type.${work.type}`)}</TableCell>
-              <TableCell>{work.type === "hashtag" ? "#" + work.tag : ""}</TableCell>
-              <TableCell>
-                <div className="flex flex-row gap-2 flex-nowrap">
-                  <Button onClick={() => up(work.id)} variant="ghost" size="icon" className="w-6 h-6">
-                    <ChevronUpIcon className="w-4 h-4"/>
-                  </Button>
-                  <Button onClick={() => down(work.id)} variant="ghost" size="icon" className="w-6 h-6">
-                    <ChevronDownIcon className="w-4 h-4"/>
-                  </Button>
-                  <WorkFormDialog
-                    work={work}
-                    trigger={
-                      <Button variant="ghost" size="icon" className="w-6 h-6">
-                        <SquarePenIcon className="w-4 h-4"/>
-                      </Button>
-                    }
-                  />
-                  <Button onClick={() => remove(work.id)} variant="ghost" size="icon" className="w-6 h-6">
-                    <TrashIcon className="w-4 h-4"/>
-                  </Button>
-                </div>
-              </TableCell>
+    <div className="w-full ">
+      <ScrollArea className=" h-[calc(100vh-150px)] ">
+        <Table>
+          <TableHeader className="relative">
+            <TableRow>
+              <TableHead>종류</TableHead>
+              <TableHead>상세</TableHead>
+
+              <TableHead className="w-[100px]"></TableHead>
             </TableRow>
-          ))
-        }
-      </TableBody>
-    </Table>
+          </TableHeader>
+          <TableBody>
+            {workList.map((work) => (
+              <TableRow key={work.id}>
+                <TableCell>{t(`work.type.${work.type}`)}</TableCell>
+                <TableCell>{work.type === 'hashtag' ? '#' + work.tag : ''}</TableCell>
+                <TableCell>
+                  <div className="flex flex-row gap-2 flex-nowrap">
+                    <Button
+                      onClick={() => up(work.id)}
+                      variant="ghost"
+                      size="icon"
+                      className="w-6 h-6"
+                    >
+                      <ChevronUpIcon className="w-4 h-4" />
+                    </Button>
+                    <Button
+                      onClick={() => down(work.id)}
+                      variant="ghost"
+                      size="icon"
+                      className="w-6 h-6"
+                    >
+                      <ChevronDownIcon className="w-4 h-4" />
+                    </Button>
+                    <WorkFormDialog
+                      work={work}
+                      trigger={
+                        <Button variant="ghost" size="icon" className="w-6 h-6">
+                          <SquarePenIcon className="w-4 h-4" />
+                        </Button>
+                      }
+                    />
+                    <Button
+                      onClick={() => remove(work.id)}
+                      variant="ghost"
+                      size="icon"
+                      className="w-6 h-6"
+                    >
+                      <TrashIcon className="w-4 h-4" />
+                    </Button>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </ScrollArea>
+    </div>
   )
 }
