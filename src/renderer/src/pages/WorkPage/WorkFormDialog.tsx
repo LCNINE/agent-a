38c41d1854780b18bc4b34299a28755ee0031170
JungSik Jeong <wndtlr1024@gmail.com>
2025-02-27 15:27:22 +0900

    refactor: 사용자가 작업 '해시태그'로 했으면 다음 작업추가할 때 해시태그가 먼저 나오게 설정 + 해시태그 이전 값 입력되어있던거 입력 값 삭제처리

--- src/renderer/src/pages/WorkPage/WorkFormDialog.tsx
+++ src/renderer/src/pages/WorkPage/WorkFormDialog.tsx
@@ -1,17 +1,17 @@
-import React, { ReactNode, useState } from "react"
+import React, { ReactNode, useState } from 'react'
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { useForm, useWatch } from "react-hook-form"
-import { workSchema, WorkSchema } from "./schema"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useWorkStore, Work } from "@/store/workStore"
-import { useTranslation } from "react-i18next"
+  DialogTrigger
+} from '@/components/ui/dialog'
+import { useForm, useWatch } from 'react-hook-form'
+import { workSchema, WorkSchema } from './schema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useWorkStore, Work } from '@/store/workStore'
+import { useTranslation } from 'react-i18next'
 import {
   Form,
   FormControl,
@@ -19,18 +19,23 @@ import {
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { generateUUID } from "@/utils/uuid"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { Input } from "@/components/ui/input"
-import { HashIcon } from "lucide-react"
-import { Button } from "@/components/ui/button"
-
+  FormMessage
+} from '@/components/ui/form'
+import { generateUUID } from '@/utils/uuid'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
+import { Input } from '@/components/ui/input'
+import { HashIcon } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 type WorkFormDialogProps = {
-  work: Work | null,
-  trigger?: ReactNode,
+  work: Work | null
+  trigger?: ReactNode
 }
 export function WorkFormDialog({ work, trigger }: WorkFormDialogProps) {
   const [open, setOpen] = useState(false)
@@ -38,33 +43,32 @@ export function WorkFormDialog({ work, trigger }: WorkFormDialogProps) {
   const { t } = useTranslation()
 
   const form = useForm<WorkSchema>({
-    mode: "onChange",
+    mode: 'onChange',
     resolver: zodResolver(workSchema),
     defaultValues: work ?? {
-      type: "feed",
+      type: 'feed'
     }
   })
   const watchedType = useWatch({
     control: form.control,
-    name: "type"
+    name: 'type'
   })
 
-  console.log('watchedType: ', watchedType)
-  console.error('form.formState.errors: ',form.formState.errors)
-  
   const { add, update } = useWorkStore()
 
   function onSubmit(values: WorkSchema) {
-    if (work == null) { // create
+    if (work == null) {
+      // create
       add({
         id: generateUUID(),
-        ...values,
+        ...values
       })
-    }
-    else { // update
+      values.type === 'hashtag' && form.reset({ type: 'hashtag' }) // 사용자 친화적인것을 위해..
+    } else {
+      // update
       update({
         id: work.id,
-        ...values,
+        ...values
       })
     }
 
@@ -73,24 +77,20 @@ export function WorkFormDialog({ work, trigger }: WorkFormDialogProps) {
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      {
-        trigger
-        ? (
-          <DialogTrigger asChild>
-            { trigger }
-          </DialogTrigger>
-        ) 
-        : (
-          <DialogTrigger asChild>
-            <Button size="sm">
-              {work == null ? t("workForm.trigger.create") : t("workForm.trigger.update") }
-            </Button>
-          </DialogTrigger>
-        )
-      }
+      {trigger ? (
+        <DialogTrigger asChild>{trigger}</DialogTrigger>
+      ) : (
+        <DialogTrigger asChild>
+          <Button size="sm">
+            {work == null ? t('workForm.trigger.create') : t('workForm.trigger.update')}
+          </Button>
+        </DialogTrigger>
+      )}
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>{work == null ? t("workForm.title.create") : t("workForm.title.update") }</DialogTitle>
+          <DialogTitle>
+            {work == null ? t('workForm.title.create') : t('workForm.title.update')}
+          </DialogTitle>
         </DialogHeader>
 
         <Form {...form}>
@@ -100,7 +100,7 @@ export function WorkFormDialog({ work, trigger }: WorkFormDialogProps) {
               name="type"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>{t("workForm.type.label")}</FormLabel>
+                  <FormLabel>{t('workForm.type.label')}</FormLabel>
                   <Select onValueChange={field.onChange} value={field.value}>
                     <FormControl>
                       <SelectTrigger>
@@ -108,40 +108,36 @@ export function WorkFormDialog({ work, trigger }: WorkFormDialogProps) {
                       </SelectTrigger>
                     </FormControl>
                     <SelectContent>
-                      <SelectItem value="feed">{t("workForm.type.select.feed")}</SelectItem>
-                      <SelectItem value="hashtag">{t("workForm.type.select.hashtag")}</SelectItem>
+                      <SelectItem value="feed">{t('workForm.type.select.feed')}</SelectItem>
+                      <SelectItem value="hashtag">{t('workForm.type.select.hashtag')}</SelectItem>
                     </SelectContent>
                   </Select>
                   <FormMessage />
                 </FormItem>
               )}
             />
-            
 
-
-            {
-              watchedType  === "hashtag" && (
-                <FormField
-                  control={form.control}
-                  name="tag"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{t("workForm.tag.label")}</FormLabel>
-                      <FormControl>
-                        <div className="flex flex-row items-center gap-2">
-                          <HashIcon/>
-                          <Input {...field} />
-                        </div>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              )
-            }
+            {watchedType === 'hashtag' && (
+              <FormField
+                control={form.control}
+                name="tag"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t('workForm.tag.label')}</FormLabel>
+                    <FormControl>
+                      <div className="flex flex-row items-center gap-2">
+                        <HashIcon />
+                        <Input {...field} />
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
 
             <Button type="submit">
-              { work == null ? t("workForm.submit.create") : t("workForm.submit.update") }
+              {work == null ? t('workForm.submit.create') : t('workForm.submit.update')}
             </Button>
           </form>
         </Form>
