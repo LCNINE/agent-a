1dd727b3f6e0fbf003d20768ee72a15796ab50b1
JungSik Jeong <wndtlr1024@gmail.com>
2025-05-27 11:35:32 +0900

    feat: 로그인시 이메일 저장 기능

--- src/renderer/src/pages/LoginPage/LoginForm.tsx
+++ src/renderer/src/pages/LoginPage/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { cn } from '@/utils/tailwind'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
@@ -22,20 +22,40 @@ import {
 import { Link } from '@tanstack/react-router'
 import { Spinner } from '@/components/spinner'
 import { toast } from 'sonner'
+import { Checkbox } from '@renderer/components/ui/checkbox'
 
 export default function LoginForm({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
   const { t } = useTranslation()
   const supabase = useCreateClient()
+  const [rememberEmail, setRememberEmail] = useState(false)
 
   const form = useForm<LoginSchema>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
-      email: '',
+      email: localStorage.getItem('rememberedEmail') || '',
       password: ''
     }
   })
 
+  useEffect(() => {
+    const savedEmail = localStorage.getItem('rememberedEmail')
+    if (savedEmail) {
+      setRememberEmail(true)
+    }
+  }, [])
+
+  const handleRememberEmailChange = (checked: boolean) => {
+    setRememberEmail(checked)
+    if (!checked) {
+      localStorage.removeItem('rememberedEmail')
+    }
+  }
+
   async function onSubmit(values: LoginSchema) {
+    if (rememberEmail) {
+      localStorage.setItem('rememberedEmail', values.email)
+    }
+
     const { error } = await supabase.auth.signInWithPassword({
       email: values.email,
       password: values.password
@@ -91,6 +111,15 @@ export default function LoginForm({ className, ...props }: React.ComponentPropsW
                   />
                 </div>
 
+                <div className="flex items-center gap-2">
+                  <Checkbox
+                    id="remember"
+                    checked={rememberEmail}
+                    onCheckedChange={handleRememberEmailChange}
+                  />
+                  <Label htmlFor="remember">email 저장하기</Label>
+                </div>
+
                 <Button
                   type="submit"
                   className="w-full"
@@ -98,6 +127,7 @@ export default function LoginForm({ className, ...props }: React.ComponentPropsW
                 >
                   {form.formState.isSubmitting ? <Spinner /> : t('loginForm.submit')}
                 </Button>
+
               </div>
               <div className="mt-4 text-center text-sm">
                 {t('loginForm.noAccount')}
