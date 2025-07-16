dec9d0a53d28c86a74fbbc8c2d56bd0546135795
JungSik Jeong <wndtlr1024@gmail.com>
2025-07-16 15:25:56 +0900

    refactor: header에 Authorization추가

--- src/main/agent/common/fetchers.ts
+++ src/main/agent/common/fetchers.ts
@@ -1,7 +1,11 @@
 import axios, { AxiosResponse } from 'axios'
 
 const axiosClient = axios.create({
-  validateStatus: (status) => status < 500
+  validateStatus: (status) => status < 500,
+  headers: {
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzemRnYm1nd25heGJ5ZWtxb25zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgzODAxMDcsImV4cCI6MjA1Mzk1NjEwN30.S4fGG1sv9drG9f04ejWCpmeGyrLkRTdXnxq_UaZzlUg'
+  }
 })
 
 type GenerateCommentReq = {
@@ -33,7 +37,7 @@ export async function callGenerateComments(params: GenerateCommentReq) {
     AxiosResponse<GenerateCommentRes>,
     GenerateCommentReq
   >('https://xszdgbmgwnaxbyekqons.supabase.co/functions/v1/generate-comment', params)
-  
+
   return response.data
 }
 
