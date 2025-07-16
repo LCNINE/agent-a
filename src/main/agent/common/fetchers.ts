31a2a042268dbc57009669db314621eb53ddc983
JungSik Jeong <wndtlr1024@gmail.com>
2025-07-16 16:01:58 +0900

    refactor: header 제거

--- src/main/agent/common/fetchers.ts
+++ src/main/agent/common/fetchers.ts
@@ -1,11 +1,7 @@
 import axios, { AxiosResponse } from 'axios'
 
 const axiosClient = axios.create({
-  validateStatus: (status) => status < 500,
-  headers: {
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzemRnYm1nd25heGJ5ZWtxb25zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgzODAxMDcsImV4cCI6MjA1Mzk1NjEwN30.S4fGG1sv9drG9f04ejWCpmeGyrLkRTdXnxq_UaZzlUg'
-  }
+  validateStatus: (status) => status < 500
 })
 
 type GenerateCommentReq = {
