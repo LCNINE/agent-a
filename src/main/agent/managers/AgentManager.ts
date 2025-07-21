4e6b876ceee76be0a387685f01ea96bedcb247dc
jungsikJeong <wndtlr1024@gmail.com>
2025-07-21 11:53:27 +0900

    안쓰이는 로그 제거

--- src/main/agent/managers/AgentManager.ts
+++ src/main/agent/managers/AgentManager.ts
@@ -471,7 +471,6 @@ export class AgentManager {
                     'a.x1i10hfl.xjbqb8w.x1ejq31n.x18oe1m7.x1sy0etr.xstzfhl.x972fbf.x10w94by.x1qhh985.x14e42zd.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x14z9mp.xat24cr.x1lziwak.xexx8yu.xyri2b.x18d9i69.x1c1uobl.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz._aswp._aswq._asws._aswv._aswz._asw_._asx2._a6hd'
                   ).first()
 
-                  console.log('author::', await authorLoc.textContent())
                   author = await authorLoc.textContent()
 
                   retryCount++
